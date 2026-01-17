299a8f062e888a3061835341e1c7b0334bd76627
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-17 06:48:16 +0200

    used javascript to add html displaying the outcome

--- script.js
+++ script.js
@@ -32,31 +32,41 @@ function convertCurrency() {
   switch (foreignCurrency) {
     case "Naira":
       console.log(message.concat(naira).concat(currency[0]));
+      message = message.concat(naira).concat(currency[0]);
       break;
     case "Euros":
       console.log(message.concat(euros).concat(currency[1]));
+      message = message.concat(euros).concat(currency[1]);
       break;
     case "US dollars":
     case "Canadian dollars":
       console.log(message.concat(usDollars).concat(currency[2]));
+      message = message.concat(usDollars).concat(currency[2]);
       break;
     case "Rupees":
       console.log(message.concat(rupees).concat(currency[3]));
+      message = message.concat(rupees).concat(currency[3]);
       break;
     case "Yen":
       console.log(message.concat(yen).concat(currency[4]));
+      message = message.concat(yen).concat(currency[4]);
       break;
     case "Pounds":
       console.log(message.concat(pounds).concat(currency[5]));
+      message = message.concat(pounds).concat(currency[5]);
       break;
     case "Emalangeni":
     case "Pula":
       console.log(message.concat(emalangeni).concat(currency[6]));
+      message = message.concat(emalangeni).concat(currency[6]);
       break;
     default:
       alert(
-        "Please enter a valid currency. Note: start your currency with uppercase character."
+        "Please enter a valid currency. Note: start your currency with an uppercase character."
       );
   }
+  let convertedCurrency = document.createElement("p");
+  convertedCurrency.innerText = message;
+  document.body.appendChild(convertedCurrency);
 }
 //button.addEventListener("click", convertCurrency());... you only need to specify event once and this called the function immediately so i placed an onclick event listener directly on the button in html
