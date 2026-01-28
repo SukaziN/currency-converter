edd2cb77f49dbc9a85eca60a2cacfd02f3127a8d
SukaziN <nkosiyazisukazie@gmail.com>
2026-01-28 06:41:51 +0200

    corrected number formatting in the thousands for consistency across all browsers (by overriding the user's local settings)

--- script.js
+++ script.js
@@ -14,54 +14,65 @@ function convertCurrency() {
     "Zim Dollars",
   ];
 
-  let naira = rands * 86.21; //market value as of 20/01/26
-  let euros = rands / 19.27; //market value as of 20/01/26
-  let usDollars = rands / 16.44; //market value as of 20/01/26
-  let zimDollars = rands * 3906.07; //market value as of 20/01/26
-  let rupees = rands * 5.53; //market value as of 20/01/26
-  let yen = rands * 9.59; //market value as of 20/01/26
-  let pounds = rands / 22; //market value as of 20/01/26 (British pound)
-  let franc = rands / 20.809; //market value as of 20/01/26
+  let naira = rands * 86.21; //these conversions reflect the market value as of 20/01/26
+  let euros = rands / 19.27;
+  let usDollars = rands / 16.44;
+  let zimDollars = rands * 3906.07;
+  let rupees = rands * 5.53;
+  let yen = rands * 9.59;
+  let pounds = rands / 22; // (British pound)
+  let franc = rands / 20.809;
   let message = `${rands}  Rands is equivalent to: `;
   switch (foreignCurrency) {
     case "Naira":
-      //console.log(message.concat(naira).concat(currency[0]));
-      message = message.concat(naira.toPrecision(3)).concat(currency[0]);
+      message = message
+        .concat(naira.toLocaleString("en-US")) //groups numbers in the thousands in US english format and differentiates a comma from a decimal
+        .concat(currency[0]);
       break;
     case "Euros":
-      //console.log(message.concat(euros).concat(currency[1]));
-      message = message.concat(euros.toPrecision(3)).concat(currency[1]);
+      message = message
+        .concat(euros.toLocaleString("en-US"))
+        .concat(currency[1]);
       break;
     case "US dollars":
-    case "Canadian dollars":
-      //console.log(message.concat(usDollars).concat(currency[2]));
-      message = message.concat(usDollars.toPrecision(3)).concat(currency[2]);
+      //case "Canadian dollars": will get actual exchange rate for canadian dollars and include it
+      message = message
+        .concat(
+          usDollars.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD", //further specifies that we're working with currency and US dollars and adds the dollar symbol as a prefix to the number
+          }),
+        )
+        .concat(currency[2]);
       break;
     case "Rupees":
-      //console.log(message.concat(rupees).concat(currency[3]));
-      message = message.concat(rupees.toPrecision(3)).concat(currency[3]);
+      message = message
+        .concat(rupees.toLocaleString("en-US"))
+        .concat(currency[3]);
       break;
     case "Yen":
-      //console.log(message.concat(yen).concat(currency[4]));
-      message = message.concat(yen.toPrecision(3)).concat(currency[4]);
+      message = message.concat(yen.toLocaleString("en-US")).concat(currency[4]);
       break;
     case "Pounds":
-      //console.log(message.concat(pounds).concat(currency[5]));
-      message = message.concat(pounds.toPrecision(3)).concat(currency[5]);
+      message = message
+        .concat(pounds.toLocaleString("en-US"))
+        .concat(currency[5]);
       break;
     case "Franc":
-      //console.log(message.concat(franc).concat(currency[6]));
-      message = message.concat(franc.toPrecision(3)).concat(currency[6]);
+      message = message
+        .concat(franc.toLocaleString("en-US"))
+        .concat(currency[6]);
       break;
     case "Zim dollars":
-      message = message.concat(zimDollars.toPrecision(3)).concat(currency[7]);
+      message = message
+        .concat(zimDollars.toLocaleString("en-US"))
+        .concat(currency[7]);
     default:
       alert(
-        "Please enter a valid currency. Note: start your currency with an uppercase character."
+        "Please enter a valid currency. Note: start your currency with an uppercase character.",
       );
   }
   let convertedCurrency = document.createElement("p");
   convertedCurrency.innerText = message;
   document.body.appendChild(convertedCurrency);
 }
-//button.addEventListener("click", convertCurrency());... you only need to specify event once and this called the function immediately so i placed an onclick event listener directly on the button in html
